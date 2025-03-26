977da0fd5333aca48d0de4693df2999aa5f653d6
Murtaza0318 <murtazasaleem2004@gmail.com>
2025-03-26 13:02:44 +0500

    solved deployment bugs 4

--- src/app/about/[id]/page.tsx
+++ src/app/about/[id]/page.tsx
@@ -1,13 +1,19 @@
-import React from 'react'
+'use client';
 
-function page({params}) {
-  return (
-    <div className='flex justify-center items-center min-h-screen'>
-      <div className='flex justify-center items-center flex-col'>
-       <h1 className='text-slate-500 text-xl '>{params.id}</h1>
-      </div>
-    </div>
-  )
+import React from 'react';
+
+interface PageProps {
+  params: {
+    id: string;
+  };
 }
 
-export default page
+const Page: React.FC<PageProps> = ({ params }) => (
+  <div className="flex justify-center items-center min-h-screen">
+    <div className="flex justify-center items-center flex-col">
+      <h1 className="text-slate-500 text-xl">{params.id}</h1>
+    </div>
+  </div>
+);
+
+export default Page;
